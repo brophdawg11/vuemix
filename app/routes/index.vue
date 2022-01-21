cc0d4f8bb4b222ae889c721d19668ba2cc90e514
Matt Brophy <matt@brophy.org>
2022-01-20 22:09:48 -0500

    remove unused code

--- app/routes/index.vue
+++ app/routes/index.vue
@@ -50,12 +50,11 @@
 </template>
 
 <script>
-import { computed, onMounted, ref } from 'vue';
+import { computed, ref } from 'vue';
 
 import {
   useActionData,
   useLoaderData,
-  useVuemixCtx,
   VuemixForm,
 } from '../../vuemix/index.mjs';
 
@@ -83,37 +82,15 @@ export default {
     VuemixForm,
   },
   setup() {
-    const vuemixCtx = useVuemixCtx();
     const loaderData = useLoaderData();
     const actionData = useActionData();
 
     const count = ref(loaderData.value?.count);
     const text = computed(() => actionData.value?.text);
 
-    const isMounted = ref(false);
-    onMounted(() => {
-      isMounted.value = true;
-    });
-
-    async function onSubmit() {
-      const formData = new FormData(document.getElementsByTagName('form')[0]);
-      const res = await fetch(`${window.location.pathname}?_action=xxx`, {
-        method: 'post',
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams(formData),
-      });
-      const data = await res.json();
-      // TODO: Move form submission to <Form>
-      vuemixCtx.actionData = data;
-    }
-
     return {
       count,
       text,
-      isMounted,
-      onSubmit,
     };
   },
 };
